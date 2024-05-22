05f931a8ef5b75b30939c0a43fa41e155b20b531
md nurullah <nurullah7733@gmail.com>
2024-05-22 12:42:59 +0600

    test

--- src/types/UserData.ts
+++ src/types/UserData.ts
@@ -1,6 +1,6 @@
 export type UserData = {
-    email: string;
-    email_verified: boolean;
-    phone_verified: boolean;
-    sub: string;
+  email: string;
+  email_verified: boolean;
+  phone_verified: boolean;
+  sub: string;
 };
