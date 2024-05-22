2a03c7261b649a9c106518fb3c03765c74cb14c6
md nurullah <nurullah7733@gmail.com>
2024-05-22 12:45:58 +0600

    delete many folder

--- src/types/Conversation.ts
+++
@@ -1,6 +0,0 @@
-export type Conversation = {
-    conversation_id: string;
-    created_at: string;
-    user_id: string;
-    summary: string;
-};
--- src/types/UserData.ts
+++
@@ -1,6 +0,0 @@
-export type UserData = {
-  email: string;
-  email_verified: boolean;
-  phone_verified: boolean;
-  sub: string;
-};
