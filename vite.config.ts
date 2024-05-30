f883091fdba90c38c5fb9d37728fe83bb8169edf
alshahabrezvi <alshahabrezvi200@gmail.com>
2024-05-30 08:44:23 +0600

    route fixing test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "public", // This is important for Vercel
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
