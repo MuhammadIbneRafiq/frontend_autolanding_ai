3f81d36c2a990385d9e244882680b0872b8781ae
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-08-20 17:18:28 +0530

    changes in backend request

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -44,7 +44,7 @@ export const useAuth = () => {
       }
 
       const response = await axios.post(
-        "http://localhost:3000/user/role",
+        "https://www.autolanding.ai/user/role",
         {},
         {
           headers: {
