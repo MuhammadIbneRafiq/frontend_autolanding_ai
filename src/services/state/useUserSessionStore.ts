2602bbb8a8441b22f178076b2aebec9d2fae6f00
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-08-20 17:11:40 +0530

    google auth with role based completed

--- src/services/state/useUserSessionStore.ts
+++ src/services/state/useUserSessionStore.ts
@@ -3,11 +3,15 @@ import { create } from "zustand";
 
 type UserSessionStore = {
     user: UserData | undefined;
+    role: string | undefined;
+    setRole: (role: string | undefined)=>void;
     setUser: (user: UserData | undefined) => void;
 };
 
 export const useUserSessionStore = create<UserSessionStore>((set) => ({
     user: undefined,
+    role:undefined,
+    setRole: (role) => set({ role }),
     setUser: (user) => set({ user }),
 }));
 
