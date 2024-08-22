934a7093b384f85a3530cb290719c9fea00bc14a
Himesh Parashar <himeshparashar424@gmail.com>
2024-08-23 00:03:42 +0530

    chore: Add URL field to ResultItemProps and update TwitterSearch component

--- src/components/TwitterSearch.tsx
+++ src/components/TwitterSearch.tsx
@@ -5,19 +5,22 @@ interface ResultItemProps {
   name: string;
   tweet: string;
   profile: string;
+  url: string;
 }
 
 const TwitterSearch = ({ tweetResult }: { tweetResult: ResultItemProps[] }) => {
   return (
     <div className="flex gap-2 overflow-x-auto whitespace-nowrap max-w-full">
       {tweetResult.map((result: ResultItemProps) => (
-        <ResultCard
-          key={result.id}
-          id={result.id}
-          name={result.name}
-          tweet={result.tweet}
-          profile={result.profile}
-        />
+        <a href={result.url} target="_blank" rel="noreferrer" key={result.id}>
+          <ResultCard
+            key={result.id}
+            id={result.id}
+            name={result.name}
+            tweet={result.tweet}
+            profile={result.profile}
+          />
+        </a>
       ))}
     </div>
   );
--- src/constants/test.ts
+++ src/constants/test.ts
@@ -6,6 +6,7 @@ export const tweetResult = [
       "Looking for new freelance opportunities in web development. Available to start immediately. #freelance #webdev",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "https://www.linkedin.com/in/jamae/",
   },
   {
     id: 2,
@@ -14,6 +15,7 @@ export const tweetResult = [
       "Experienced graphic designer open for freelance projects. DM me if you need a creative mind! #freelance #graphicdesign",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "https://www.linkedin.com/in/vasilijesimic/",
   },
   {
     id: 3,
@@ -22,6 +24,7 @@ export const tweetResult = [
       "Currently looking for freelance software development work. Specialize in Python and Django. #freelance #Python",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "https://www.linkedin.com/in/filip-wauters/",
   },
   {
     id: 4,
@@ -30,55 +33,56 @@ export const tweetResult = [
       "Available for freelance copywriting. Get in touch for quality content that resonates with your audience. #freelance #copywriting",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "https://www.linkedin.com/in/thomaspalermo",
   },
-  {
-    id: 5,
-    name: "John Wilson",
-    tweet:
-      "Looking for a new project! Freelance full-stack developer with experience in React and Node.js. #freelance #fullstack",
-    profile:
-      "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
-  },
-  {
-    id: 6,
-    name: "Sophia Martinez",
-    tweet:
-      "Freelance digital marketing expert available to boost your brand's online presence. Let's connect! #freelance #digitalmarketing",
-    profile:
-      "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
-  },
-  {
-    id: 7,
-    name: "James Anderson",
-    tweet:
-      "Offering freelance photography services. Capturing moments that matter. Contact me for collaborations. #freelance #photography",
-    profile:
-      "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
-  },
-  {
-    id: 8,
-    name: "Olivia Taylor",
-    tweet:
-      "Available for freelance UX/UI design projects. Creating user-centered designs that engage and convert. #freelance #uxdesign",
-    profile:
-      "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
-  },
-  {
-    id: 9,
-    name: "Daniel Hernandez",
-    tweet:
-      "Freelance mobile app developer open to new projects. Let's build something amazing together! #freelance #mobiledev",
-    profile:
-      "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
-  },
-  {
-    id: 10,
-    name: "Emma Lee",
-    tweet:
-      "Looking for freelance writing gigs. Experienced in blog posts, articles, and creative content. #freelance #writing",
-    profile:
-      "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
-  },
+  // {
+  //   id: 5,
+  //   name: "John Wilson",
+  //   tweet:
+  //     "Looking for a new project! Freelance full-stack developer with experience in React and Node.js. #freelance #fullstack",
+  //   profile:
+  //     "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+  // },
+  // {
+  //   id: 6,
+  //   name: "Sophia Martinez",
+  //   tweet:
+  //     "Freelance digital marketing expert available to boost your brand's online presence. Let's connect! #freelance #digitalmarketing",
+  //   profile:
+  //     "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+  // },
+  // {
+  //   id: 7,
+  //   name: "James Anderson",
+  //   tweet:
+  //     "Offering freelance photography services. Capturing moments that matter. Contact me for collaborations. #freelance #photography",
+  //   profile:
+  //     "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+  // },
+  // {
+  //   id: 8,
+  //   name: "Olivia Taylor",
+  //   tweet:
+  //     "Available for freelance UX/UI design projects. Creating user-centered designs that engage and convert. #freelance #uxdesign",
+  //   profile:
+  //     "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+  // },
+  // {
+  //   id: 9,
+  //   name: "Daniel Hernandez",
+  //   tweet:
+  //     "Freelance mobile app developer open to new projects. Let's build something amazing together! #freelance #mobiledev",
+  //   profile:
+  //     "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+  // },
+  // {
+  //   id: 10,
+  //   name: "Emma Lee",
+  //   tweet:
+  //     "Looking for freelance writing gigs. Experienced in blog posts, articles, and creative content. #freelance #writing",
+  //   profile:
+  //     "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+  // },
 ];
 
 export const tweetResultProjects = [
@@ -89,6 +93,7 @@ export const tweetResultProjects = [
       "In need of a skilled web developer for a new project. Freelancers, please get in touch! #hiring #webdev",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 2,
@@ -97,6 +102,7 @@ export const tweetResultProjects = [
       "Looking to hire a creative graphic designer for our upcoming campaign. Freelancers, DM me your portfolio! #hiring #graphicdesign",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 3,
@@ -105,6 +111,7 @@ export const tweetResultProjects = [
       "Seeking a freelance Python developer for a short-term project. Contact me if interested! #hiring #Python",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 4,
@@ -113,6 +120,7 @@ export const tweetResultProjects = [
       "Need a talented copywriter for a series of blog posts. Freelancers, reach out! #hiring #copywriting",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 5,
@@ -121,6 +129,7 @@ export const tweetResultProjects = [
       "Hiring a full-stack developer for an exciting project. Freelancers with React and Node.js experience, let's connect! #hiring #fullstack",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 6,
@@ -129,6 +138,7 @@ export const tweetResultProjects = [
       "Looking for a digital marketing expert to help grow our brand. Freelancers, please reach out! #hiring #digitalmarketing",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 7,
@@ -137,6 +147,7 @@ export const tweetResultProjects = [
       "In need of a freelance photographer for an upcoming event. Contact me for details! #hiring #photography",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 8,
@@ -145,6 +156,7 @@ export const tweetResultProjects = [
       "Seeking a freelance UX/UI designer for a product redesign. Freelancers, let’s chat! #hiring #uxdesign",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 9,
@@ -153,6 +165,7 @@ export const tweetResultProjects = [
       "Looking to hire a freelance mobile app developer. Exciting projects ahead! #hiring #mobiledev",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
   {
     id: 10,
@@ -161,5 +174,6 @@ export const tweetResultProjects = [
       "In search of a freelance writer for various content needs. Writers, please send over your samples! #hiring #writing",
     profile:
       "https://pbs.twimg.com/profile_images/1624781151809466368/tnuASsdY_400x400.jpg",
+    url: "",
   },
 ];
